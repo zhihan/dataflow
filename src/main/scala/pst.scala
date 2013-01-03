ccb8098f5785993392309c50e574561feb7c4b35
U-zhi-hp\zhi <zhi@zhi-hp.(none)>
2013-01-02 21:26:35 -0500

    Program structure tree

--- src/main/scala/pst.scala
+++ src/main/scala/pst.scala
@@ -3,15 +3,46 @@
 package my.ir.pst
 
 import my.ir._
+import my.se._
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
 
 abstract class Region
-case class BasicRegion(body:List[Statement]) extends Region
-case class ChainRegion(body:List[Region]) extends Region
-case class IfElseRegion(b1:Region, b2:Region) extends Region
+case class BasicRegion() extends Region
+case class ChainRegion() extends Region
+case class IfElseRegion() extends Region
 
-
+class Pst(val root:TreeNode, r:Region ) {
+  val regions = Map(root.id -> r) // mutable
+}
 
 object Pst {
+  val tf = new TreeNodeFactory()
+  def basic(l:List[Statement]) = {
+    val root = tf.make()
+    new Pst(root, BasicRegion())
+   }
+  def chain(l:List[Pst]) = {
+    val root = tf.make()
+    val pst = new Pst(root, ChainRegion())
+    for (i <- l) {
+      root.addChild(i.root)
+      pst.regions ++= i.regions
+    }
+    pst
+  }
+  def ifElse(b1:Pst, b2:Pst) = {
+    val root =tf.make()
+    root.addChild(b1.root)
+    root.addChild(b2.root)
+    val pst = new Pst(root, IfElseRegion()) 
+    pst.regions++= b1.regions
+    pst.regions++= b2.regions
+    pst
+  }
+}
+
+object PstFactory {
     /*
      */
   private def isIf(s:Statement) = {
@@ -21,12 +52,38 @@ object Pst {
     }
   }
 
-  def createPst(l:List[Statement]):Region = {
+  def createPst(l:List[Statement]):Pst = {
     if (l.forall( s => !isIf(s))) {
-      BasicRegion(l)
+      Pst.basic(l)
     } else {
-      // 
-      ChainRegion(List[Region]())
+      val listOfRegion = ListBuffer[Pst]()  
+      val listOfStmt = ListBuffer[Statement]()
+      def finishBasicRegion() {
+	if (!listOfStmt.isEmpty) {
+	  // Create a new Basic Region and
+	  // add append it to list of region
+	  val l = listOfStmt.toList
+	  listOfStmt.clear()
+	  val br = Pst.basic(l)
+	  listOfRegion.append(br)
+	}
+      }
+      
+      for (s <- l) {
+	s match {
+	  case IfElse(_,b1,b2) => {
+	    finishBasicRegion()
+	    val r1 = createPst(b1)
+	    val r2 = createPst(b2)
+	    listOfRegion.append( Pst.ifElse(r1,r2))
+	  }
+	  case _ => {
+	    listOfStmt.append(s)
+	  }
+	}
+      }
+      finishBasicRegion()
+      Pst.chain(listOfRegion.toList)
     }
   }
     
--- src/test/scala/test_pst.scala
+++ src/test/scala/test_pst.scala
@@ -17,13 +17,35 @@ package my.ir.PstTest {
       }"""
       val p = new ParseAndCreateIR()
       val (ast, _) = p.parse(a)
-      val root = Pst.createPst(ast.body)
-      val isBasic = root match {
-	case BasicRegion(_) => true
+      val r = PstFactory.createPst(ast.body)
+      val isBasic = r.regions(r.root.id) match {
+	case BasicRegion() => true
 	case _ => false
       }
       assert(isBasic)
       
+    }
+
+    test("Chain region") {
+      val a = """function((),(), chain)
+      {
+	=(y,@x);
+        if(@b) { 
+	  =(z,@y);
+        } else {
+	  =(z,@x);
+	}
+      }"""
+      val p = new ParseAndCreateIR()
+      val (ast, _) = p.parse(a)
+      val r = PstFactory.createPst(ast.body)
+      assert( r.regions(r.root.id) == ChainRegion())
+      val children = r.root.children
+      assert(children.length == 2)
+      assert(children.exists(c =>
+	r.regions(c.id) == IfElseRegion()))
+      assert(children.exists(c =>
+	r.regions(c.id) == BasicRegion()))
     }
   }
 
