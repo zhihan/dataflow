917fae68d3e6478524d801d62e879736c512ae96
Zhi Han <zhi.han@gmail.com>
2013-09-06 15:58:07 -0400

    Added compact and expand

--- src/main/scala/bus_tree.scala
+++ src/main/scala/bus_tree.scala
@@ -4,6 +4,9 @@ package my.se
 // dataflow dependence with the dataflow graph. 
 
 import my.se._
+import scala.collection.immutable.Set // Set of indices
+import scala.collection.immutable.Range 
+import scala.annotation.tailrec
 
 abstract sealed class BusElement 
 
@@ -29,10 +32,186 @@ case class Bus (val name: String, val children:List[BusElement]) extends BusElem
       (acc, c) => acc + (
         c match {
           case _:AtomicElement => 1
-          case b:Bus => b.nDescendants
+          case b:Bus => b.nDescendants + 1
         }
       )
     }
+  lazy val size = nDescendants +1
+
+  /* Indexing: bus elements are assumed to be DFS-indexed,
+   * this allows use of a single id to find the element */
+  def get(n:Int) : BusElement = {
+    def find(n:Int, l:List[BusElement]):BusElement = {
+      val h = l.head
+      (h,n) match {
+        case (b:Bus, _) if n < b.size  =>
+          b.get(n)
+        case (b:Bus, _) =>
+          find( n - b.size, l.tail)
+        case (a:AtomicElement, 0) =>
+          h 
+        case (_:AtomicElement, n) =>
+          find(n-1, l.tail)
+      }
+    }
+
+    if (n > nDescendants+1) {
+      throw new RuntimeException("Index larger than number of descendants")
+    } else {
+      n match {
+        case 0 => this
+        case 1 => children.head
+        case _ if n < 0 => throw new RuntimeException("Index less than 0")
+        case _ => find(n-1, children)
+      }
+    }
+  }
+
+  def isDescendant(c:Int, p:Int) = {
+    var pb = get(p)
+    pb match {
+      case b: Bus => (c > p && c <= p + b.nDescendants)
+      case _: AtomicElement => false
+    }
+  }
+
+  def leavesId :Set[Int] = {
+    @tailrec
+    def leafIds(start:Int, acc:Set[Int], l:List[BusElement]): Set[Int] = {
+      l match {
+        case h::t =>
+          h match {
+            case _:AtomicElement => leafIds(start+1, acc+start, t )
+            case b:Bus => leafIds(start + b.size, acc ++ lId(b, start), t)
+          }
+        case Nil => acc
+      }
+    }
+    // Local function for recursion
+    def lId(b:Bus, n:Int):Set[Int] = leafIds(n+1, Set[Int](), b.children)
+
+    lId(this, 0)
+  }
+
+  def descendantsId(start:Int):Range = start+1 to start + nDescendants
+    
+
+  // A set of sub bus elements are represented as set of indices
+  def leavesIdOf(s:Set[Int]) = {
+    @tailrec
+    def leafIds(start:Int, acc:Set[Int], 
+                sel:(Int)=>Boolean, l:List[BusElement]): Set[Int] = {
+      l match {
+        case h::t =>
+          h match {
+            case _:AtomicElement => 
+              if (sel(start)) 
+                leafIds(start+1, acc+start, sel, t)
+              else
+                leafIds(start+1, acc, sel, t)
+            case b:Bus => 
+              if (sel(start))
+                leafIds(start + b.size, acc ++ allIds(b, start), sel, t)
+              else
+                leafIds(start + b.size, acc, sel, t)
+          }
+        case Nil => acc
+      }
+    }
+
+
+    // Local function for recursion
+    def allIds(b:Bus, n:Int):Set[Int] = leafIds(n+1, Set[Int](), 
+                                                (_:Int)=> true, b.children)
+
+    leafIds(1, Set[Int](), (x:Int)=> s.contains(x), children )
+  }
 
   
+  // Expand the set to all indices
+  def expand(s:Set[Int]) = {
+    @tailrec
+    def allIds(start:Int, acc:Set[Int], 
+                sel:(Int)=>Boolean, l:List[BusElement]): Set[Int] = 
+      l match {
+        case h::t =>
+          h match {
+            case _:AtomicElement => 
+              if (sel(start)) 
+                allIds(start+1, acc+start, sel, t)
+              else
+                allIds(start+1, acc, sel, t)
+            case b:Bus => 
+              if (sel(start))
+                allIds(start + b.size, acc ++ (
+                  start to start + b.nDescendants),
+                        sel, t)
+              else 
+                allIds(start + b.size, acc ++ expandBus(b, start, sel), sel, t)
+          }
+        case Nil => acc
+      }
+    
+
+    def expandBus(b:Bus, start:Int, sel:(Int)=>Boolean ):Set[Int] = {
+      // After recursion, decide whether bus is included
+      val res = allIds(start+1, Set[Int](), sel, b.children)
+      val all = (start+1 to start + b.nDescendants).forall{
+        (x:Int) => res.contains(x)
+      }
+      if (all) res + start else res
+    }
+
+    if (s.contains(0)) 
+      (0 to size-1).toSet
+    else {
+      val res = allIds(1, Set[Int](), (x:Int)=>s.contains(x), children)
+      val all = (1 to nDescendants).forall{
+        (x:Int) => res.contains(x)
+      }
+      if (all) res + 0 else res
+    }
+  }
+
+  // Compact: if all children is listed, only store the root
+  def compact(s:Set[Int]) = {
+    @tailrec 
+    def cIds(start:Int, acc:Set[Int], r:Set[Int], 
+         accIncluded:Boolean, l:List[BusElement]): (Set[Int], Boolean) = 
+           if (r.isEmpty) 
+             (acc, accIncluded && l.isEmpty)
+           else 
+             l match {
+               case h::t =>
+                   h match {
+                     case _:AtomicElement => 
+                       if (r.contains(start))
+                         cIds(start+1, acc+start, r-start, accIncluded, t)
+                       else 
+                         cIds(start+1, acc, r, false, t)
+                     case b:Bus => {
+                       val (s, all) = compactBus(b, start, r)
+                       val nR = r -- (start to start + b.nDescendants)
+                       cIds(start + b.size, acc ++ s, nR, accIncluded && all, t)
+                     }
+                   }
+               case Nil =>
+                 (acc, accIncluded)
+             }
+
+    def compactBus(b:Bus, start:Int, r:Set[Int]): (Set[Int],Boolean) = {
+      if (r.contains(start)) 
+        (Set(start), true)
+      else {
+        val (s, all) = cIds(start+1, Set[Int](), r, true, b.children)
+        if (all) (Set(start), true) else (s,false)
+      }
+    }
+    
+    val (res, _) = compactBus(this, 0, s)
+    res
+  }
+}
+
+object Bus {
 }
--- src/test/scala/test_bus.scala
+++ src/test/scala/test_bus.scala
@@ -12,7 +12,8 @@ class BusTest extends FunSuite {
     val bus = Bus("ab", List(a,b))
     assert(bus.width == 3)
 
-    val busb = Bus("abb", List(bus, b))
+    val c = AtomicElement("b", 2)
+    val busb = Bus("abc", List(bus, c))
     assert(busb.width == 5)
   }
 
@@ -28,7 +29,86 @@ class BusTest extends FunSuite {
     val b = AtomicElement("b",2)
     val bus = Bus("ab", List(a,b))
     assert(bus.nDescendants == 2)
+    val c = AtomicElement("b", 2)
+    val busb = Bus("abc", List(bus, c))
+    assert(busb.nDescendants == 4)
+  }
+
+  test("Bus indexing") {
+    val a = AtomicElement("a",1)
+    val b = AtomicElement("b",2)
+    val bus = Bus("ab", List(a,b))
+    val c = AtomicElement("b",2)
     val busb = Bus("abb", List(bus, b))
-    assert(busb.nDescendants == 3)
+
+    assert(busb.get(0) == busb)
+    assert(busb.get(1) == bus)
+    assert(busb.get(2) == a)
+    assert(busb.get(3) == b)
+    assert(busb.get(4) == c)
+  }
+
+  test("Bus descendant using indexing") {
+    val a = AtomicElement("a",1)
+    val b = AtomicElement("b",2)
+    val bus = Bus("ab", List(a,b))
+    val c = AtomicElement("b",2)
+    val busb = Bus("abb", List(bus, b))
+
+    assert(busb.isDescendant(1,1) == false)
+    assert(busb.isDescendant(2,0) == true)
+    assert(busb.isDescendant(3,1) == true)
+    assert(busb.isDescendant(4,1) == false)
+  }
+  
+  test("Bus leaves indexing") {
+    val a = AtomicElement("a",1)
+    val b = AtomicElement("b",2)
+    val bus = Bus("ab", List(a,b))
+    val c = AtomicElement("b",2)
+    val busb = Bus("abb", List(bus, b))
+
+    assert(busb.leavesId == Set(2,3,4))
+  }
+
+  test("Bus leaves indexing for subset") {
+    val a = AtomicElement("a",1)
+    val b = AtomicElement("b",2)
+    val bus = Bus("ab", List(a,b))
+    val c = AtomicElement("b",2)
+    val busb = Bus("abb", List(bus, b))
+
+    assert(busb.leavesIdOf(Set(1,4)) == Set(2,3,4))
+    assert(busb.leavesIdOf(Set(1)) == Set(2,3))
+    assert(busb.leavesIdOf(Set(4)) == Set(4))
+  }
+
+  test("Bus leaves indexing for expand") {
+    val a = AtomicElement("a",1)
+    val b = AtomicElement("b",2)
+    val bus = Bus("ab", List(a,b))
+    val c = AtomicElement("b",2)
+    val busb = Bus("abb", List(bus, b))
+
+    assert(busb.expand(Set(1,4)) == Set(0,1,2,3,4))
+    assert(busb.expand(Set(2,3,4)) == Set(0,1,2,3,4))
+    assert(busb.expand(Set(1)) == Set(1,2,3))
+    assert(busb.expand(Set(1,2)) == Set(1,2,3))
+    assert(busb.expand(Set(4)) == Set(4))
+    assert(busb.expand(Set(0)) == Set(0,1,2,3,4))
+  }
+
+  test("Bus leaves indexing for compact") {
+    val a = AtomicElement("a",1)
+    val b = AtomicElement("b",2)
+    val bus = Bus("ab", List(a,b))
+    val c = AtomicElement("b",2)
+    val busb = Bus("abb", List(bus, b))
+
+    assert(busb.compact(Set(1,4)) == Set(0))
+    assert(busb.compact(Set(2,3,4)) == Set(0))
+    assert(busb.compact(Set(1)) == Set(1))
+    assert(busb.compact(Set(1,2)) == Set(1))
+    assert(busb.compact(Set(4)) == Set(4))
   }
 }
