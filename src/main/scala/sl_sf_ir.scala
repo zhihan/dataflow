b787a325f207bab6713aefe3428c7e1d192bf41a
Zhi Han <zhi.han@gmail.com>
2014-03-28 17:14:31 -0400

    Added junction

--- src/main/scala/sl_sf_ir.scala
+++ src/main/scala/sl_sf_ir.scala
@@ -10,36 +10,50 @@ import scala.collection.mutable.ArrayBuffer
 /** Syntax part */
 sealed abstract class StateflowObject extends AnyRef with HasId
 
-sealed abstract class Statelike extends StateflowObject
+sealed abstract class StateLike extends StateflowObject
+{
+  def toDotString: String
+}
 
 case class State(override val id:Int, 
-  val name:String, subStates: List[State] = List()) extends Statelike
+  val name:String, subStates: List[StateLike] = List()) extends StateLike
 {
   def toDotString = f"$id%d[shape=Mrecord]"+"[label=\"" +
     f"$name%s" + "\"]"
+
+  // Ordered out-going transitions
+  val out = ArrayBuffer[Transition]()
 }
 
-case class Junction(override val id:Int) extends Statelike
+case class Junction(override val id:Int) extends StateLike
 {
   val out = ArrayBuffer[Transition] ()
   val in = ArrayBuffer[Transition] ()
+  def toDotString = f"$id%d[shape=circle]" + "[label=\"\"]"
 }
 
 sealed abstract class Transition extends StateflowObject
 { def toDotString: String }
 
 case class NormalTransition(override val id: Int, 
-  val source:Statelike, val destination:Statelike,
+  val source:StateLike, val destination:StateLike,
   val condition: Option[Condition], val event: Option[Event],
   val cAction: Option[Action], val tAction: Option[Action] ) 
     extends Transition
 {
-  override def toDotString = f"${source.id}%d -> ${destination.id}%d" + 
-    "[label=\"" + f"$id%d" + "\"]"
+  override def toDotString = {
+    val condStr = condition match {
+      case Some(c) => f"[${c.text}%s]"
+      case None => ""
+    }
+
+    f"${source.id}%d -> ${destination.id}%d" +
+    "[label=\"" +  f"$id%d" + condStr + "\"]"
+  }
 }
 
 case class DefaultTransition(override val id: Int,
-  val destination: Statelike) extends Transition
+  val destination: StateLike) extends Transition
 {
   override def toDotString = "default[style=invisible]\n" + 
   f"default -> ${destination.id}%d" + "[label=\"" + 
@@ -54,7 +68,8 @@ case class Data(override val id:Int) extends StateflowObject
 {
 }
 
-class Condition {
+class Condition(val text: String) {
+
 }
 
 class Action {
@@ -63,7 +78,7 @@ class Action {
 class Function {
 }
 
-class Chart(val states: List[State], 
+class Chart(val states: List[StateLike], 
   val transitions:List[Transition] ) {
   // No known mutable data members yet
 
--- src/test/scala/test_sl_sf_ir.scala
+++ src/test/scala/test_sl_sf_ir.scala
@@ -4,16 +4,42 @@ import sl.ir.sf._
 import org.scalatest.FunSuite
 
 class SFIRTest extends FunSuite {
+  // On <==> Off switch
   def createSimpleChart = {
     val onState = State(1, "on")
     val offState = State(2, "off")
     val a = NormalTransition(3, onState, offState, 
       None, None, None, None)
+    onState.out.append(a)
     val b = NormalTransition(4, offState, onState, 
       None, None, None, None)
+    offState.out.append(b)
     val c = DefaultTransition(5, onState)
     new Chart(List(onState, offState),
       List(a, b, c))
+  }
+
+  def createFlowchart = {
+    val a = Junction(1)
+    val b = Junction(2)
+    val c = Junction(3)
+    val d = Junction(4)
+
+    val cond = new Condition("u>0")
+    val ab = NormalTransition(5, a, b, 
+      condition= Some(cond), None, None, None) 
+    val ac = NormalTransition(6, a, c,
+      None, None, None, None)
+    a.out.append(ab)
+    a.out.append(ac)
+
+    val bd = NormalTransition(7, b, d,
+      None, None, None, None)
+    val cd = NormalTransition(8, c, d,
+      None, None, None, None)
+    val aa = DefaultTransition(9, a)
+    
+    new Chart(List[StateLike](a,b,c,d), List(ab,ac,bd,cd, aa))
   }
 
   test("Creating SF IR") {
@@ -21,4 +47,12 @@ class SFIRTest extends FunSuite {
     val s = chart.toDotString
     assert(s.length > 10)
   }
+
+  test("Creating Flowchart IR") {
+    val chart = createFlowchart
+    val s = chart.toDotString
+    println(s)
+    assert(s.length > 10)
+  }
+
 }
